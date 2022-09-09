5bcd8a4dd4e7343518fd6cd124ce79c54690f2e1
Santosh Rai <rai.santosh55@gmail.com>
2022-09-09 11:11:24 +0545

    adds automation and javascript to display the window in app mode

---
+++ run.bat
@@ -0,0 +1,3 @@
+start chrome.exe --app="http://127.0.0.1:8000/" --new-window
+python manage.py runserver
+start chrome /new-window http://127.0.0.1:8000/
--- searcher/views.py
+++ searcher/views.py
@@ -5,12 +5,5 @@ from . import searcher
 
 
 def searchbox(request):
-    # worker = []
-    # if not keyboard.is_pressed(hotkey):
-    #     ideal = {'term': "",
-    #              'final_term': "Click shift+F1 to search the term in your clipboard."}
-    #     return render(request, 'searcher/home.html', {'worker': ideal})
-
     worker = searcher.searcher()
-
     return render(request, 'searcher/home.html', {'worker': worker})
